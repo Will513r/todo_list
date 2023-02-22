267c315223c5035aac8a8c6c83fc9eea9d1c8c0a
will513 <will513r@gmail.com>
2023-02-22 13:30:38 -0500

    created functionality to adding to the to do list

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var items = ["Buy food", "Cook food"];
+
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -24,12 +26,13 @@ var options = {
 var day = today.toLocaleDateString("en-US", options);
 
 
-   res.render("list", { kindOfDay: day, newListItem: item});
+   res.render("list", { kindOfDay: day, newListItems: items});
 
 });
 
 app.post("/", function (req, res) {
   var item = req.body.newItem;
+  items.push(item);
   res.redirect("/")
 });
 
